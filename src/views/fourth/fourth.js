e9d29aba9b283ac744523b5e2637a02febdd141c
Adriel Itamar <adrielitamar@gmail.com>
2023-08-27 10:30:28 -0300

    feat: fourth page script

--- src/views/fourth/fourth.js
+++ src/views/fourth/fourth.js
@@ -1,3 +1,13 @@
+function getForm() {
+    const thirdForm = JSON.parse(localStorage.getItem('form'))
+    
+    if (thirdForm.report){
+        document.getElementById('text').value = thirdForm.report
+    }
+}
+
+getForm()
+
 document.addEventListener('DOMContentLoaded', function () {
     let report = ""
     const text = document.getElementById('text')
@@ -8,16 +18,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const submitButton = document.getElementById('submitButton')
     submitButton.addEventListener('click', function(event) {
         event.preventDefault()
+        const checkForm = JSON.parse(localStorage.getItem('form'))
         if (report.length <= 10)
-            alert("The field must be filled to proceed.")
+            checkForm.report ? report = checkForm.report : alert("The field must be filled to proceed.")
     
-            else {
-                const first = JSON.parse(localStorage.getItem('form'))
-                const form = {...first, "report": report }
-                localStorage.setItem("form", JSON.stringify(form));
-                window.location.href = "../fifth/index.html";
-            }   
-        
+        else {
+            const first = JSON.parse(localStorage.getItem('form'))
+            const form = {...first, "report": report }
+            localStorage.setItem("form", JSON.stringify(form));
+            window.location.href = "../fifth/index.html";
+        }   
         
     });
 });
