71eae0de714b230709909dbbf58bac385c4ab2ea
Adriel Itamar <adrielitamar@gmail.com>
2023-08-28 09:33:15 -0300

    feat: removing the form in localstorage

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
 if (window.innerWidth > 640){
     document.getElementById('startButton').textContent = "Just Go Ahead"
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const startButton = document.getElementById('startButton')
+    startButton.addEventListener('click', function(event) {
+        localStorage.removeItem('form')
+    });
+});
